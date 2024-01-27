18af4fd6473a18f39942901a24b8fe26445f3b15
garber <garbfbatista@gmaail.com>
2024-01-27 17:08:56 -0400

    [UI] Animacion y images

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,17 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   darkMode: "class", // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        border: "border 4s ease infinite",
+      },
+      keyframes: {
+        border: {
+          "0%, 100%": { backgroundPosition: "0% 50%" },
+          "50%": { backgroundPosition: "100% 50%" },
+        },
+      },
+    },
   },
   plugins: [],
 };
